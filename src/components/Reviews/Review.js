a954f368cbb9bab8fc0d235a00d3ac7ed8cdeb55
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-01-04 00:34:01 +0600

    review page added

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKey = Object.keys(savedCart);
+        const productQuantity = Object.values(savedCart);
+        const cartProduct = productKey.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        }
+        )
+        setCart(...cart+cartProduct)
+    }, [])
+    console.log(cart);
     return (
         <div>
+            <h2>{cart.length}</h2>
             <h2>Lets Play Guys! </h2>
         </div>
     );
