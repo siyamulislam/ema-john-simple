76fac565a13f4d7dde6fbaaf33d57c13aa11d7e9
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-15 21:43:13 +0600

    single product added throught fake DB

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,19 @@
-import React from 'react';
 
+import React from 'react';
+import fakeData from '../../fakeData';
 const Inventory = () => {
+    const handelAddProduct=()=>{
+        fetch('http://localhost:5000/addProducts',{
+            method:'POST',
+            headers:{'COntent_TYPE': 'application/json'},
+            body:JSON.stringify(fakeData[0])
+
+        })
+    }
     return (
         <div>
             <h1>Hi Inventory</h1>
+            <button onClick={handelAddProduct}>Add Product</button>
         </div>
     );
 };
