780cc5a1184c5f9c7bbe16f1c99640bdd9dad72a
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-19 22:04:46 +0600

    loading CP useState

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,3 +1,4 @@
+import { CircularProgress } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Product from '../Product/Product';
@@ -5,18 +6,22 @@ import Product from '../Product/Product';
 const ProductDetails = () => {
     const { productKey } = useParams();
     const [product, setProduct] = useState({})
+    const [loading , setLoading]= useState(true)
     useEffect(() => {
         fetch('https://powerful-castle-25731.herokuapp.com/product/' + productKey,)
             .then(res => res.json())
             .then(data => {
                 setProduct(data)
+                setLoading(false)
             })
     }, [productKey])
     // const product= fakeData.find(pd=> pd.key===productKey); 
     return (
         <div>
-            <h3>{productKey}Product Details</h3>
-            <Product showAddCart={false} item={product}> </Product>
+            {loading? <h3>Loading Product Details</h3>:  <h3>{productKey}Product Details</h3>}
+            {loading?  <CircularProgress /> :   <Product showAddCart={false} item={product}> </Product>}
+          
+          
         </div>
     );
 };
