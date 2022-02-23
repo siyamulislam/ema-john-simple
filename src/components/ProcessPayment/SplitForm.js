5f198fd696edc07b78e47c36ce5537187807b92a
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-23 16:07:05 +0600

    formation

--- src/components/ProcessPayment/SplitForm.js
+++ src/components/ProcessPayment/SplitForm.js
@@ -9,7 +9,7 @@ import {
 import './Style.css'
 
 const useOptions = () => {
-  const fontSize ='16px'
+  const fontSize = '16px'
   const options = useMemo(
     () => ({
       style: {
@@ -33,12 +33,12 @@ const useOptions = () => {
   return options;
 };
 
-const SplitForm = ({handelPayment}) => {
+const SplitForm = ({ handelPayment }) => {
   const stripe = useStripe();
   const elements = useElements();
   const options = useOptions();
-  const [success, setSuccess]=useState('');
-  const [error, setError]=useState('');
+  const [success, setSuccess] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async event => {
     event.preventDefault();
@@ -54,15 +54,15 @@ const SplitForm = ({handelPayment}) => {
       card: elements.getElement(CardNumberElement)
     });
     console.log("[PaymentMethod]", payload);
-    if(payload.error){setSuccess(''); setError(payload.error.message);}
-    if(payload.paymentMethod) {setError(''); setSuccess( payload.paymentMethod.id); handelPayment(payload.paymentMethod.id,payload.paymentMethod.card.brand)}
+    if (payload.error) { setSuccess(''); setError(payload.error.message); }
+    if (payload.paymentMethod) { setError(''); setSuccess(payload.paymentMethod.id); handelPayment(payload.paymentMethod.id, payload.paymentMethod.card.brand) }
     console.log("[PaymentMethod]", payload);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Card number ............................ 
+        Card number ............................
         <CardNumberElement
           options={options}
           onReady={() => {
@@ -115,9 +115,8 @@ const SplitForm = ({handelPayment}) => {
           }}
         />
       </label> <br />
-      {error? <p className="text-danger"> <small>{error}</small> </p>: success?
-      <p className="text-success"> <small>Payment Completed! <span className="text-primary">{" TrxID: "+success}</span></small> </p>:''}
-    
+      {error ? <p className="text-danger"> <small>{error}</small> </p> : success ?
+        <p className="text-success"> <small>Payment Completed! <span className="text-primary">{" TrxID: " + success}</span></small> </p> : ''}
       <button type="submit" disabled={!stripe}>
         Pay
       </button>
